1ea80ca106f2adbda99f37d0189b1c9d20070cd0
Raghav Seth <sethraghav12345@gmail.com>
2025-02-05 01:23:59 +0530

    added home page

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,12 @@ const app = express()
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-    }
-)
-  
+})
+
+app.get('/home', (req, res) => {
+    res.send("This is home page")
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}`)
-    }
-)
+})
