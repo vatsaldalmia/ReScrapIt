8ef4b2e9b1588d491f7669cc7fe0d46adb427ded
Vatsal Dalmia <vatsaldalmia@Vatsals-MacBook-Air-2.local>
2025-02-05 02:22:33 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,10 @@ app.get('/home', (req, res) => {
     res.send("This is home page")
 })
 
+app.get('/twitter', (req, res) => {
+    res.send("This is twitter page")
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}`)
 })
