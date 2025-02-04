89641e7b5f70d184397063dbb186e10676961d7c
Raghav Seth <sethraghav12345@gmail.com>
2025-02-05 02:22:48 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,10 @@ app.get('/home', (req, res) => {
     res.send("This is home page")
 })
 
+app.get('/youtube', (req, res) => {
+    res.send("This is youtube page")
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}`)
 })
