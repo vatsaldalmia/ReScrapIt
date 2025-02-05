f73458b873d0865e119d35dbe780d63bd1de3adc
Raghav Seth <sethraghav12345@gmail.com>
2025-02-05 18:01:29 +0530

    initial commit

---
+++ backend/src/models/user.models.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        pan: {
+            type: String,
+            required: true,
+            unique: true,
+        }
+    },
+    {timestamps : true}
+)
+
+module.exports = User = mongoose.model('user',userSchema)
